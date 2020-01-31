52d0eb349d5ed79185e6426efc775f06b9785eba
Lucas Goudin <lucas.goudin@etu.u-bordeaux.fr>
2020-01-31 21:02:54 +0100

    Zoom and pan

--- app/src/main/java/com/lucasgoudin/pictura/MainActivity.java
+++ app/src/main/java/com/lucasgoudin/pictura/MainActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.chrisbanes.photoview.PhotoView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PhotoView photoView = (PhotoView) findViewById(R.id.photo_view);
 
     }
 
