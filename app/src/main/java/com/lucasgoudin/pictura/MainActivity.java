c6dc3216e1962b72c701e2e980497697d1346d00
lgoudin <lucas.goudin@etu.u-bordeaux.fr>
2020-02-24 14:17:41 +0100

    Saved pictures have the correct date

--- app/src/main/java/com/lucasgoudin/pictura/MainActivity.java
+++ app/src/main/java/com/lucasgoudin/pictura/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.FileProvider;
 
 import android.Manifest;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -517,12 +518,14 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if(requestCode == SAVE_IMAGE) {
             if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 try {
-                    String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + R.string.app_name + "/";
+                    String path = Environment.getExternalStorageDirectory() + File.separator + R.string.app_name;
                     File dir = new File(path);
                     if(!dir.exists()){
                         dir.mkdirs();
@@ -540,7 +543,15 @@ public class MainActivity extends AppCompatActivity {
                     selectedFilter.apply(full_image_result);
                     full_image_result.compress(Bitmap.CompressFormat.JPEG, 90, out);
 
-                    MediaStore.Images.Media.insertImage(MainActivity.this.getContentResolver(),photoFile.getAbsolutePath(), photoFile.getName(), photoFile.getName());
+                    ContentValues values = new ContentValues();
+                    values.put(MediaStore.Images.Media.TITLE, timeStamp);
+                    values.put(MediaStore.Images.Media.DISPLAY_NAME, timeStamp);
+                    values.put(MediaStore.Images.Media.DESCRIPTION, "");
+                    values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpg");
+                    values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis());
+                    values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+                    values.put(MediaStore.Images.Media.DATA, photoFile.getAbsolutePath());
+                    MainActivity.this.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
 
                     out.flush();
                     out.close();
@@ -553,7 +564,6 @@ public class MainActivity extends AppCompatActivity {
             } else {
                 Toast.makeText(getApplicationContext(), R.string.noPermissions, Toast.LENGTH_LONG).show();
             }
-            return;
         }
     }
 }
